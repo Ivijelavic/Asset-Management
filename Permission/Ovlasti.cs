e8dc1b829a20d9ed7c3f5592fc2fd3240cc8323a
Ivijelavic <73886325+Ivijelavic@users.noreply.github.com>
2022-04-01 10:51:38 +0200

    Add files via upload

---
+++ Permission/Ovlasti.cs
@@ -0,0 +1,52 @@
+﻿using MVCappCoreWeb.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCappCoreWeb.Models;
+using System; 
+
+namespace MVCappCoreWeb.Permission
+{
+    public  class Ovlasti
+    {
+        private readonly UserManager<WebUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public Ovlasti(UserManager<WebUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+            public async Task<List<UserRolesViewModel>> getUloge(string userId)
+        {
+            var viewModel = new List<UserRolesViewModel>();
+            var user = await _userManager.FindByIdAsync(userId);
+            foreach (var role in _roleManager.Roles)
+            {
+                var userRolesViewModel = new UserRolesViewModel
+                {
+                    RoleName = role.Name
+                };
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    if (role.Name != "SuperAdmin")
+                    {
+                        userRolesViewModel.Selected = true;
+                    }
+
+                }
+                else
+                {
+                    userRolesViewModel.Selected = false;
+                }
+                viewModel.Add(userRolesViewModel);
+            }
+
+
+            return viewModel;
+        }
+
+    }
+}
